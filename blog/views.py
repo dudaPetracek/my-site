8ea341d1105e1e0798c318b8711787706bc6d500
Duda Petracek <157414482+dudaPetracek@users.noreply.github.com>
2025-09-30 09:33:36 -0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models.post import Post
 
 def post_list(request):
-    posts = Post.objects.all()  # busca todos os posts
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.all()
+    return render(request, "blog/post_list.html", {"posts": posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, "blog/post_detail.html", {"post": post})
