78d73afb5e8fc767cec1428f17119b7de2ab5e0c
Duda Petracek <157414482+dudaPetracek@users.noreply.github.com>
2025-12-04 10:19:37 -0300

    Refactor post views and add post_view function

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from .models.post import Post
+from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, "blog/post_list.html", {"posts": posts})
+    posts = Post.objects.all().order_by('-id')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    return render(request, "blog/post_detail.html", {"post": post})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_view(request, pk=None):
+    if pk:
+        return post_detail(request, pk)
+    return post_list(request)
